4662fa06b4f13d52ad33cfa5fe793e75d01e10b2
Teddy Wing <ted@teddywing.com>
2014-03-25 01:28:53 -0400

    Generate a file download link to easily save the data

--- chrome-get-urls-from-tabs-in-windows.js
+++ chrome-get-urls-from-tabs-in-windows.js
@@ -1,4 +1,6 @@
 var textarea = document.getElementById('copy-area');
+var create_download_link;
+var generate_filename;
 
 chrome.windows.getAll({populate:true},function(windows){
 	var w_index = 0;
@@ -18,4 +20,33 @@ chrome.windows.getAll({populate:true},function(windows){
 		
 		w_index++;
 	});
+	
+	create_download_link(textarea.value);
 });
+
+
+create_download_link = function(text) {
+	var download_link = document.createElement('a');
+	download_link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	download_link.setAttribute('download', generate_filename());
+	download_link.innerHTML = 'Download file';
+	
+	document.querySelector('body').appendChild(download_link);
+};
+
+
+generate_filename = function() {
+	var d = new Date();
+	var date_string = 
+		d.getFullYear() 
+		+ '' 
+		+ ('0' + (d.getMonth() + 1)).slice(-2) 
+		+ '' 
+		+ ('0' + d.getDate()).slice(-2) 
+		+ '-' 
+		+ ('0' + d.getHours()).slice(-2) 
+		+ 'h' 
+		+ d.getMinutes();
+	
+	return 'chrome-tabs-' + date_string + '.txt';
+};
